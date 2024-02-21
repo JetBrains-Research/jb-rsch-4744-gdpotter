38f3e07dcd8bbb390c3dab553598eefa4c5b80f8
Greg Potter <gdpotter123@gmail.com>
2024-02-20 23:03:12 -0500

    Add test for Position.move

--- src/test/java/org/jetbrains/assignment/PositionTest.java
+++ src/test/java/org/jetbrains/assignment/PositionTest.java
@@ -6,5 +6,16 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PositionTest {
 
+    @Test
+    void move_goesToCorrectPosition() {
+        assertEquals(new Position(0, 5), new Position(0, 4).move(new Movement(Direction.NORTH ,1)));
+        assertEquals(new Position(0, 5), new Position(0, 8).move(new Movement(Direction.SOUTH ,3)));
+        assertEquals(new Position(2, 5), new Position(2, 4).move(new Movement(Direction.NORTH ,1)));
+        assertEquals(new Position(2, 5), new Position(2, 8).move(new Movement(Direction.SOUTH ,3)));
 
+        assertEquals(new Position(5, 0), new Position(4, 0).move(new Movement(Direction.EAST ,1)));
+        assertEquals(new Position(5, 0), new Position(8, 0).move(new Movement(Direction.WEST ,3)));
+        assertEquals(new Position(5, 2), new Position(4, 2).move(new Movement(Direction.EAST ,1)));
+        assertEquals(new Position(5, 2), new Position(8, 2).move(new Movement(Direction.WEST ,3)));
+    }
 }
